a896c9dd1e5a14b60f7f51103b2ddb484515a091
Waleed Hifthi <waleedhifthi@alMacBook-Air-alkhas-bWaleed.local>
2025-10-27 19:35:33 +0300

    fix(pdf): use chromium.executablePath() and puppeteer-core

--- pages/api/generate-pdf.js
+++ pages/api/generate-pdf.js
@@ -225,7 +225,7 @@ export default async function handler(req, res) {
 
     
 // واستبدل الإطلاق بهذا:
-const executablePath = await chromium.executablePath;
+const executablePath = await chromium.executablePath();
 const browser = await puppeteer.launch({
   args: chromium.args,
   defaultViewport: chromium.defaultViewport,
